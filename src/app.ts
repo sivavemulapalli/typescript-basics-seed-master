12e5e9d3834d3dac8ed219f9aeb9f56618721245
Chaitanya Vemulapalli <chaitanya.vemulapalli2@t-mobile.com>
2019-10-22 13:04:47 +0530

    boolean types, boolean literals

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('Hey TypeScript!');
+const pizzas: number = 5;
+
+function offerDiscount(orders: number):boolean{
+  return orders > 3;
+}
+
+if(offerDiscount(pizzas)){
+  console.log(`You're are entitled to a discount`);
+} else {
+  console.log(`Order more than 3 pizzas for a discount!`);
+}
