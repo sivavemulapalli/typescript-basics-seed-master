af31865fa799d380e7da88f6c9c17133448ecde6
Chaitanya Vemulapalli <chaitanya.vemulapalli2@t-mobile.com>
2019-10-22 17:40:00 +0530

    type aliases

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('Hey TypeScript!');
+type Size = "small" | "medium" | "large";
+
+type Callback = (size: Size) => void;
+
+let pizzaSize: Size = "small";
+
+const selectSize: Callback = x => {
+  pizzaSize = x;
+};
+
+selectSize("medium");
