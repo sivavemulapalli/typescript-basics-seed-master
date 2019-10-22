79900ce317e252c4df03d08b81be82b691c62371
Chaitanya Vemulapalli <chaitanya.vemulapalli2@t-mobile.com>
2019-10-22 12:50:41 +0530

    number types, arguments and functions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('Hey TypeScript!');
+const pizzaCost: number = 10;
+
+const pizzaToppings: number = 2;
+
+function calculatePrice(cost:number, toppings:number):number {
+    return cost + 1.5 * toppings;
+}
+
+
+const cost:number = calculatePrice(pizzaCost, pizzaToppings);
+
+console.log(`Pizza costs: ${cost}`);
