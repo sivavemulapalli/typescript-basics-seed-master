4fd7be408f041538bbb2865521b6d361b0011859
Chaitanya Vemulapalli <chaitanya.vemulapalli2@t-mobile.com>
2019-10-22 17:04:57 +0530

    object types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('Hey TypeScript!');
+let pizza: { name: string; price: number; getName(): string };
+
+pizza = {
+  name: "Plain Old Pepperoni",
+  price: 20,
+  getName() {
+    return pizza.name;
+  }
+};
+
+console.log(pizza.getName());
