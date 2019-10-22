074b42a1fd1c6ae1e594f7faf960e50dc67fd8df
Chaitanya Vemulapalli <chaitanya.vemulapalli2@t-mobile.com>
2019-10-22 12:58:50 +0530

    string type and string literals

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('Hey TypeScript!');
+const coupon: string = 'pizza25';
+
+function normalizeCoupon(code:string): string{
+  return code.toUpperCase();
+}
+
+const couponMessage: string = `Final coupon is ${normalizeCoupon(coupon)}`;
+
+console.log(couponMessage);
