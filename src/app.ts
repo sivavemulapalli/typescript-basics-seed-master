1f98b7d75d187f66148aa314ea54482892a44036
Chaitanya Vemulapalli <chaitanya.vemulapalli2@t-mobile.com>
2019-10-22 16:47:41 +0530

    function types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-console.log('Hey TypeScript!');
+let sumOrder: (price: number, quantity: number) => number;
+
+sumOrder = (x, y) => x * y;
+
+const sum = sumOrder(25, 2);
+
+console.log(`Total sum: ${sum}`);
