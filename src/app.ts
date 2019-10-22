0e6aab35a2c62dd3e6a6dc7ef51e35d919ce05c3
Chaitanya Vemulapalli <chaitanya.vemulapalli2@t-mobile.com>
2019-10-22 16:54:42 +0530

    function and optional arguments

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hey TypeScript!');
+let sumOrder: (price: number, quantity?: number) => number;
+
+sumOrder = (x, y) => {
+  if(y) {
+   return x * y
+  } else {
+    return x;
+  }
+
+const sum = sumOrder(25);
+
+sumOrder(25, 2);
+
+console.log(`Total sum: ${sum}`);
