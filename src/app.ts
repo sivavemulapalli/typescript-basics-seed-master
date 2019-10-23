9271bef6f0a166b494542419bc213607c196411d
Chaitanya Vemulapalli <chaitanya.vemulapalli2@t-mobile.com>
2019-10-23 23:44:27 +0530

    type-assertions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('Hey TypeScript!');
+type Pizza = { name: string; toppings: number };
+
+const pizza: Pizza = { name: "Blazing Inferno", toppings: 5 };
+
+const serialized = JSON.stringify(pizza);
+
+function getNameFromJSON(obj: string) {
+  return (JSON.parse(obj) as Pizza).name;
+}
+
+getNameFromJSON(serialized);
