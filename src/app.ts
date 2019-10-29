4c67a9135c6a9f2c330994644af76295302a150a
Chaitanya Vemulapalli <chaitanya.vemulapalli2@t-mobile.com>
2019-10-29 11:11:38 +0530

    setters and getters

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
+class Sizes {
+  constructor(public sizes: string[]) {}
+
+  set availableSizes(sizes: string[]) {
+    this.sizes = sizes;
+  }
+
+  get availableSizes() {
+    return this.sizes;
+  }
+}
+
+const sizes = new Sizes(["small", "medium"]);
+
+//invoke getter
+console.log(sizes.availableSizes);
+
+//invoke setter
+sizes.availableSizes = ["medium", "large"];
+
+console.log(sizes.availableSizes);
+
 class Pizza {
   public toppings: string[] = [];
 
@@ -11,4 +33,3 @@ class Pizza {
 const pizza = new Pizza("Pepperoni");
 
 pizza.addTopping("pepperoni");
-pizza.name = "ABC";
