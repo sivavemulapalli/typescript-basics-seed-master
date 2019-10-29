d053f94562561ad9246311376a800e50b6d3b4dd
Chaitanya Vemulapalli <chaitanya.vemulapalli2@t-mobile.com>
2019-10-29 09:37:56 +0530

    interfaces with function types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('Hey TypeScript!');
+interface Pizza {
+  name: string;
+  sizes: string[];
+  getAvailableSizes(): string[];
+}
+
+let pizza: Pizza;
+
+function createPizza(name: string, sizes: string[]): Pizza {
+  return {
+    name,
+    sizes,
+    getAvailableSizes() {
+      return this.sizes;
+    }
+  };
+}
+
+pizza = createPizza("Pepperoni", ["small", "medium"]);
