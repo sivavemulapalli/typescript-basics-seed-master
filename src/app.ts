6ad5cf7d95f18af8252dd67c554c606cc496fcb0
Chaitanya Vemulapalli <chaitanya.vemulapalli2@t-mobile.com>
2019-10-29 10:03:26 +0530

    understanding class and constructors

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('Hey TypeScript!');
+class Pizza {
+  name: string;
+  toppings: string[] = [];
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  addTopping(topping: string) {
+    this.toppings.push(topping);
+  }
+}
+
+const pizza = new Pizza("Pepperoni");
+
+pizza.addTopping("pepperoni");
+
+console.log(pizza);
