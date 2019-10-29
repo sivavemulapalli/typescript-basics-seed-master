0151bc72c0f2ab968a44d415a34813da1fa1b46c
Chaitanya Vemulapalli <chaitanya.vemulapalli2@t-mobile.com>
2019-10-29 09:31:59 +0530

    creating interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hey TypeScript!');
+interface Pizza {
+  name: string;
+  sizes: string[];
+}
+
+let pizza: Pizza;
+
+function createPizza(name: string, sizes: string[]): Pizza {
+  return {
+    name,
+    sizes
+  };
+}
+
+pizza = createPizza("Pepperoni", ["small", "medium"]);
